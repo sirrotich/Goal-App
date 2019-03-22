0a93696ce349925dc6e18e826b1e74fc28cd50f3
sirrotich <rotichtitus12@gmail.com>
2019-03-22 10:32:03 +0300

    Auto-generated commit

--- out-tsc/src/app/goal.spec.js
+++
@@ -1,7 +0,0 @@
-import { Goal } from './goal';
-describe('Goal', function () {
-    it('should create an instance', function () {
-        expect(new Goal()).toBeTruthy();
-    });
-});
-//# sourceMappingURL=goal.spec.js.map
