fdf8d1d34fb42c8dac05d497bb0b73b1f678e1e4
sirrotich <rotichtitus12@gmail.com>
2019-03-22 10:37:43 +0300

    Trigger rebuild

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -17,6 +17,7 @@ export class GoalComponent implements OnInit {
 
   ]
 
+ 
   deleteGoal(isComplete,index){
     if (isComplete){
         let toDelete=confirm(`Are you sure you want to delete ${this.goals[index].name}`)
